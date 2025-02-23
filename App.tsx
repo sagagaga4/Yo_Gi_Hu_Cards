2b40e553c66663b5855bf0e48e904bae8da3ba0f
‪SAGI <57183982+sagagaga4@users.noreply.github.com>
2025-02-23 19:51:55 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import cardImage from './assets/ally_of_justice.jpg';
 import cardImage2 from './assets/arcana_triumph_joker.jpg';
@@ -8,21 +9,38 @@ import cardImage5 from './assets/gravekeeperheadman.jpg';
 import cardImage6 from './assets/gravekeepertrap.jpg';
 import cardImage7 from './assets/ally_of_justice_catastor.jpg';
 import cardImage8 from './assets/arcana_knight_joker.jpg';
+import './assets/ruddy_rose_dragon.jpg';
 
 function App() {
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsActive(window.scrollY > 50);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
   return (
     <>
-      <div className='header'>
-        <h5 className="hello">YO GI HU</h5> 
-        <h1 className="headline">CARDS</h1>
-        <div className="info">
-          <p>collect em all!</p>
+      <div className={`header ${isActive ? 'active' : ''}`}>
+        <div className='leftsection'>
+          <h5 className="hello">YO GI HU</h5> 
+          <h1 className="headline">CARDS</h1>
+          <div className="info">
+            <p>collect em all!</p>
+          </div>
+          <div className='options'>
+            <h5 className='opt'>Upload</h5>
+            <h5 className='opt'>Stats</h5>
+            <h5 className='opt'>Create</h5>
+          </div>
         </div>
-
         {/* Search Bar */}
         <div className="search-container">
-          <FaSearch className="search-icon" />
-          <input type="text" placeholder="Search" className="search-input" />
+        <FaSearch className="search-icon" />
+        <input type="text" placeholder="Search" className="search-input" />
         </div>
 
         <button className='register'>Register</button>
@@ -38,6 +56,9 @@ function App() {
             </div>
           </div>
         ))}
+              <h1 className='infogroup2'>New Silver Cards</h1>
+              <div className='midsection'>
+              </div>
       </div>
     </>
   );
